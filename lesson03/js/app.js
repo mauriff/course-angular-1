068133a5139164b69da846c7a4a479a2cb796a74
Mauricio Ferreyra <mauricio.ferreyra@windriver.com>
2016-11-30 19:57:17 -0300

    lesson03-routes

--- lesson03/js/app.js
+++ lesson03/js/app.js
@@ -1,7 +1,15 @@
 (function () {
 'use strict';
 
-angular.module('app', ['7minWorkout']);
+angular.module('app', ['ngRoute','7minWorkout'])
+.config(function($routeProvider) {
+	 $routeProvider.when('/start',
+	 					{templateUrl:'partials/start.hml'});
+	  $routeProvider.when('/workout', 
+    	{ templateUrl: 'partials/workout.html', controller: 'WorkoutController' });
+	 $routeProvider.otherwise({ redirectTo: '/start' });
+});
+
 
 angular.module('7minWorkout', []);
 
