128ddfc46989ec282fa702b21ea3935a33031645
Mauricio Ferreyra <mauricio.ferreyra@windriver.com>
2016-12-06 20:11:38 -0300

    http

--- lesson05/01/js/controller.js
+++ lesson05/01/js/controller.js
@@ -10,16 +10,16 @@ angular.module('app.controllers', [])
     $scope.getClima = function () {
       $http({
         method: 'GET',
-        url: 'http://api.openweathermap.org/data/2.5/weather?appid=6e677030195aa712c6590e4eb6fbc78a&lang=es&units=metric&q=Cordoba' 
+        url: 'http://api.openweathermap.org/data/2.5/weather?appid=6e677030195aa712c6590e4eb6fbc78a&lang=es&units=metric&q=' + $scope.clima.ciudad 
       }).then(function successCallback(response) {
         $scope.clima.temperatura = response.data.main.temp + "°C";
-        $scope.image = "http://openweathermap.org/img/w/"+response.data.weather[0].icon+".png"
+        $scope.clima.image = "http://openweathermap.org/img/w/"+response.data.weather[0].icon+".png"
 
       }, function errorCallback(response) {
           alert("error");
       });
 };
 
-    $scope.getClima();
+
 
 });
