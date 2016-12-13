b14ca596eed5d87527bde1d6d6b3aac8f850bbc3
Mauricio Ferreyra <mauricio.ferreyra@windriver.com>
2016-12-13 19:39:45 -0300

    directives

---
+++ lesson07/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myWebApp
+ * @description
+ * # myWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myWebApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ lesson07/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myWebApp
+ */
+angular.module('myWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
